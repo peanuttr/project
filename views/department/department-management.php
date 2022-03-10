9f407d015adf0f07fb1df81d52e06fa19526b89c
Supakorn Siripisitwong <supakorn.siripisitwong@gmail.com>
2022-03-11 00:10:38 +0700

    Department

---
+++ views/department/department-management.php
@@ -0,0 +1,64 @@
+<?php
+include_once "../layout/masterpage.php";
+?>
+<div class="home-section">
+    <h1> การจัดการหน่วยงาน </h1>
+    <a class='button-17' href='./unit-add.php'> <span>เพิ่มหน่วยงาน</span> </a>
+    <table id="myTable" style="font-size:14px; width: 100%; text-align:center; border:1px;" class="table table-striped">
+        <thead>
+            <tr>
+                <th>ลำดับ</th>
+                <th>ชื่อหน่วยนับ</th>
+                <th>action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require "../../assets/config/db.php";
+            $db = new db();
+            $stmt = $db->connect()->prepare("SELECT * FROM unit");
+            $stmt->execute();
+            $number = 1;
+            while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+            <tr>
+                <td><?php echo $number ?></td>
+                <td><?php echo $result['unit_name']; ?></td>
+                <td>
+                <a href='./unit-edit.php?id=<?php echo $result['id']; ?>' class='btn btn-sm btn-success'>
+                <i class='bx bx-edit'></i>
+                </a> / 
+                <a class='del btn btn-sm btn-danger' onclick="removeUser('<?php echo $result['id']; ?>')">
+                <i class='bx bx-trash' ></i>
+            </a>
+                </td>
+            </tr>
+            
+            <?php 
+            $number+=1;
+        }
+         ?>
+        </tbody>
+    </table>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+
+    function removeUser(id) {
+        var result = confirm("Want to delete?");
+        if (result) {
+            $.ajax({
+                url: '../../assets/db/personnel/del-personnel.php',
+                type: 'POST',
+                data: {
+                    id: id
+                },
+                success: function(data) {
+                    window.location.href = "./personnel-management.php";
+                }
+            })
+        }
+    }
+</script>
--- views/department/department.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once "../layout/masterpage.php";
-?>
-<div class="home-section">
-    <h1>การจัดการข้อมูลหน่วยงาน</h1>
-</div>
