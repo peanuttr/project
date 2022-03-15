b1d7af83519c7ba068837ce09fe04e736186f947
Chitsanupong Boonserm <chitsanupongboonserm@Chitsanupongs-MacBook-Air.local>
2022-03-15 15:58:06 +0700

    pass value to js

--- views/repair-assetments/repair-assetments-add.php
+++ views/repair-assetments/repair-assetments-add.php
@@ -1,5 +1,19 @@
 <?php
 include_once "../layout/masterpage.php";
+require "../../assets/config/db.php";
+$assets = array();
+
+$db = new db;
+$stmt = $db->sqlQuery("SELECT a.*,t.assets_types_name,u.unit_name,d.department_name,m.money_source_name FROM `assets` AS a 
+                        JOIN `assets_types` as t ON a.assets_types_id = t.id 
+                        JOIN `unit` as u ON a.unit_id = u.id 
+                        JOIN `department` as d ON a.department_id = d.id 
+                        JOIN `money_source` as m ON a.money_source_id = m.id");
+$stmt->execute();
+
+foreach ($stmt->fetchAll() as $res) {
+    array_push($assets, ['assets_number' => $res['assets_number'], 'assets_name' => $res['asset_name']]);
+}
 ?>
 <div class="home-section">
     <div class="home-content">
@@ -8,21 +22,14 @@ include_once "../layout/masterpage.php";
             <div class="row">
                 <div class="col-6">
                     <label>รหัสครุภัณฑ์</label>
-                    <input list="asset-number" class="form-control" />
+                    <input type="search" list="asset-number" class="form-control" onchange="setAssetData(this.value)" />
                     <datalist id="asset-number">
                         <?php
-                        require "../../assets/config/db.php";
-                        $db = new db;
-                        $stmt = $db->sqlQuery("SELECT a.*,t.assets_types_name,u.unit_name,d.department_name,m.money_source_name FROM `assets` AS a 
-                        JOIN `assets_types` as t ON a.assets_types_id = t.id 
-                        JOIN `unit` as u ON a.unit_id = u.id 
-                        JOIN `department` as d ON a.department_id = d.id 
-                        JOIN `money_source` as m ON a.money_source_id = m.id");
-                        $stmt->execute();
-                        $asset_number = "";
-                        foreach ($stmt->fetchAll() as $res) {
+
+                        for ($i = 0; $i < count($assets); $i++) {
+
                         ?>
-                            <option value="<?php echo $res['assets_number']; ?>" />
+                            <option value="<?php echo $assets[$i]['assets_number']; ?>" />
                         <?php
                         }
                         ?>
@@ -30,25 +37,18 @@ include_once "../layout/masterpage.php";
                 </div>
                 <div class="col-6">
                     <label>ชื่อครุภัณฑ์</label>
-                    <input list="asset-name" class="form-control" />
-                    <datalist id="asset-name">
-                        <?php
-                        $stmt = $db->sqlQuery("SELECT a.*,t.assets_types_name,u.unit_name,d.department_name,m.money_source_name FROM `assets` AS a 
-                        JOIN `assets_types` as t ON a.assets_types_id = t.id 
-                        JOIN `unit` as u ON a.unit_id = u.id 
-                        JOIN `department` as d ON a.department_id = d.id 
-                        JOIN `money_source` as m ON a.money_source_id = m.id WHERE a.assets_number = '$res[assets_number]'");
-                        $stmt->execute();
-                        foreach ($stmt->fetchAll() as $res) {
-                        ?>
-                            <option value="<?php echo $res['asset_name']; ?>" />
-                        <?php
-                        }
-                        ?>
-                    </datalist>
+                    <input type="text" id="assetsname" class="form-control" />
                 </div>
 
             </div>
         </form>
     </div>
 </div>
+<script>
+    function setAssetData(value) {
+        console.log(value);
+        console.log(<?php echo json_encode($assets); ?>);
+        var assetsData = <?php echo json_encode($assets); ?>;
+        console.log(assetsData);
+    }
+</script>
