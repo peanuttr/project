67455c9d9226967e99ee86c4b9ac43072b13fd8e
chitsanupong <chitsanupong.boonserm@gmail.com>
2022-05-30 03:35:57 +0700

    fix : personel import from csv

--- assets/db/personnel/add-personnel-and-edit.php
+++ assets/db/personnel/add-personnel-and-edit.php
@@ -4,25 +4,7 @@ $db = new db();
 if (isset($_FILES['upload'])) {
     $file = $_FILES['upload']['tmp_name'];
     $file_open = fopen($file, "r");
-    $personel = array();
-    $department = array();
     $status = "ทำงาน";
-    $stmt = $db->sqlQuery("SELECT * FROM department");
-    $stmt->execute();
-    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        array_push($department, ['name' => $result['department_name'], 'id' => $result['id']]);
-    }
-    // while($csv = fgetcsv($file_open, ",")){
-
-    //     $stmt = $db->sqlQuery("SELECT * FROM department WHERE `department_name` LIKE '%$csv[0]%'");
-    //     $stmt->execute();
-    //     $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    //     echo $csv[0]." ".$csv[1]." ".$csv[3]." ".$csv[4]." ".$result['id']."<br>";
-    // }
-    // for($i = 0 ; $i < count($csv); $i++){
-    //     echo $csv[$i];
-    // }
-    $flag = false;
     while (($csv = fgetcsv($file_open, ",")) !== false) {
         $dep = $csv[0];
         $jobTitle = $csv[1];
@@ -30,31 +12,12 @@ if (isset($_FILES['upload'])) {
         $lastname = $csv[4];
         $stmt = $db->sqlQuery("SELECT * FROM department WHERE `department_name` LIKE '%$dep%'");
         $stmt->execute();
-        // $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        //     // echo "ชื่อ:" . $fisrtname . " นามสกุล: " . $lastname . " ตำแหน่ง: " . $jobTitle ."<br />";
         while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $department_id = $result['id'];
             $department_name = $result['department_name'];
-            //     // if ($department[0]['name'] == $dep) {
-            //     // array_push($personel, ['fname' => $fisrtname, 'lname' => $lastname, 'job' => $jobTitle, 'dep_name' => $dep]);
-            //     // }
-            // echo "ชื่อ:" . $fisrtname . " นามสกุล: " . $lastname . " ตำแหน่ง: " . $jobTitle . " ภาควิชา: " . $dep . " " . $department_id . "<br />";
             $stmt1 = $db->sqlQuery("INSERT INTO `personnels`(`personnel_firstname`, `personnel_lastname`, `job_title`, `status`, `department_id`) VALUES ('$fisrtname', '$lastname', '$jobTitle', '$status', '$department_id')");
             $stmt1->execute();
-            //     // print_r($personel);
         }
-        // }
-        // foreach ($personel as $val) {
-        //     echo $val['fname'] . "  " . $val['lname'] . "  " . $val['job'] . "  " . $val['dep_name'] . "<br />";
-        //     // $stmt1 = $db->sqlQuery("INSERT INTO `personnels`(`personnel_firstname`, `personnel_lastname`, `job_title`, `status`, `department_id`) VALUES ('$val[fname]', '$val[lname]', '$val[job]', '$val[fname]', '$val[fname]')");
-        //     // $stmt1->execute();
-        // }
-        // for($i = 0 ; $i < count($personel) ; $i++){
-        //     for ($k = 0; $k < count($department); $k++){
-        //         if($department[$k]['name'] == $personel[$i]['dep_name']){
-        //             echo $personel[$i]['fname'] . "  " . $personel[$i]['lname'] . "  " . $personel[$i]['job'] . "  " . $personel[$i]['dep_name']. "  " . $department[$k]['id'] . "<br />";
-        //         }
-        //     }
     }
     if ($stmt1) {
         header("location: ../../../../../project/views/personnel/personnel-management.php");
