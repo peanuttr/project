49bfd4fc0667a2a62073f536f87a86b5f3b37c39
chitsanupong <chitsanupong.boonserm@gmail.com>
2022-05-29 23:16:35 +0700

    fix

--- assets/db/report/report-pdf.php
+++ assets/db/report/report-pdf.php
@@ -13,5 +13,5 @@ $html = '<p style="text-align:center; font-size:large;">แบบบันทึ
 <p style="text-align:right;">เขียนที่ คณะอุตสาหกรรมเกษตร <br /> วันที่ 27 เดือน พฤษภาคม พ.ศ. 2565</p>
 <p style="text-align:left;">เรื่อง ขอยืมครุภัณฑ์ <br /> เรียนคณบดีคณะอุตสาหกรรมเกษตร</p>
 ';
-$pdf->writeHTML($html, true, false, true, false, '');
+$pdf->writeHTML($html);
 $pdf->Output();
