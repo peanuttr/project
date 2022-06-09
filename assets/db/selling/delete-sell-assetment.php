6a6787251086f95da5d601ec8ba370e82024de21
kapook14959 <supakorn.siripisitwong@gmail.com>
2022-06-09 10:51:05 +0700

    - Fix delete sell assetment

--- assets/db/selling/delete-sell-assetment.php
+++ assets/db/selling/delete-sell-assetment.php
@@ -4,7 +4,20 @@ $db = new db();
 
 if (isset($_POST['id'])) {
     $_id = $_POST['id'];
+    $assetId = array();
+    $stmt = $db->sqlQuery("SELECT asset_id FROM `detail_sells` WHERE `sell_id` = $_id");
+    $stmt->execute();
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        array_push($assetId, $result['asset_id']);
+    }
+
+    foreach ($assetId as $index) {
+        $stmt = $db->sqlQuery("UPDATE `assets` set `status`='อยู่ในคลัง' WHERE `id`='$index'");
+        $stmt->execute();
+    }
+
     $stmt = $db->sqlQuery("DELETE FROM detail_sells WHERE sell_id = $_id");
+    
     if ($stmt->execute()) {
         $stmt = $db->sqlQuery("DELETE FROM sells WHERE id = $_id");
         $stmt->execute();
