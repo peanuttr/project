ecc09e61d8afa8c05d127677d542579f812cc445
Chitsanupong <chitsanupong.boonserm@gmail.com>
2022-06-17 16:35:23 +0700

    create file upload csv: assets-type

---
+++ assets/db/assetment-type/add-assetment-type-csv.php
@@ -0,0 +1,15 @@
+<?php
+require "../../config/db.php";
+$db = new db();
+if (isset($_FILES['upload'])) {
+    $file = $_FILES['upload']['tmp_name'];
+    $file_open = fopen($file, "r");
+    while (($csv = fgetcsv($file_open, ",")) !== false) {
+        $assetmentTypeNumber = $csv[0];
+        $assetmentTypeName = $csv[1];
+        // echo 'No:'. $assetmentTypeNumber." Name:".$assetmentTypeName;
+        $stmt = $db->sqlQuery("INSERT INTO `assets_types`(`assets_types_name`) VALUES ('$assetmentTypeName')");
+        $stmt->execute();
+    }
+    header("location: ../../../../../project/views/assetments-type/assetments-type-management.php");
+}
