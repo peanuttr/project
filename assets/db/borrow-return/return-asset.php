0cdaa3be7bab0d1189bb88e3b2ae27d3975c776c
Chitsanupong <chitsanupong.boonserm@gmail.com>
2022-07-06 03:34:25 +0700

    validate : return

--- assets/db/borrow-return/return-asset.php
+++ assets/db/borrow-return/return-asset.php
@@ -14,10 +14,34 @@ if (isset($_POST['submit'])) {
     $borrowNumber = $_POST['numberBorrow'];
     $borrowId = '';
 
-    $stmt = $db->sqlQuery("SELECT id FROM `borrow_and_return` WHERE `number_borrow` = $borrowNumber");
+    $stmt = $db->sqlQuery("SELECT id,number_borrow FROM `borrow_and_return` WHERE `number_borrow` = $borrowNumber");
     $stmt->execute();
     while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $borrowId = $result['id'];
+    }
+
+    $detailId = array();
+    $stmt = $db->sqlQuery("SELECT asset_id FROM `detail_borrow_and_return` WHERE `borrow_and_return_id` = $borrowId AND `status` LIKE '%ถูกยืม%'");
+    $stmt->execute();
+    while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        array_push($detailId,$result['asset_id']);
+    }
+    $asstVal = array_values($_POST['assets_id']);
+    // $$detailId = array_values($detailId);
+    print_r($asstVal);
+    print_r($detailId);
+
+    $result= array_intersect($asstVal,$detailId);
+    print_r($result);
+
+    $isEqu = false;
+    if($result){
+        // echo "hello world";
+        $isEqu = true;
+    }else{
+        echo "<script>alert('ไม่พบหมายเลขครุภัณฑ์ในรายการ');
+        window.location.href='../../../../../project/views/borrow-return/return-asset.php';
+        </script>";
     }
     
     $dup = array_count_values($_POST['assets_id']);
@@ -32,18 +56,18 @@ if (isset($_POST['submit'])) {
         }
     }
 
-    if ($flag) {
-        for ($i = 0; $i < count($_POST['assets_number']); $i++) {
-            array_push($assets, ['id' => $_POST['assets_id'][$i], 'assets_number' => $_POST['assets_number'][$i], 'assets_name' => $_POST['assets_name'][$i]]);
-        }
+    if ($flag && $isEqu) {
+        // for ($i = 0; $i < count($_POST['assets_number']); $i++) {
+        //     array_push($assets, ['id' => $_POST['assets_id'][$i], 'assets_number' => $_POST['assets_number'][$i], 'assets_name' => $_POST['assets_name'][$i]]);
+        // }
 
         $personnelId = $_POST['personnelId'];
         $staffId = $_POST['staffId'];
 
-        foreach ($assets as $resp) {
-            $stmt = $db->sqlQuery("UPDATE `detail_borrow_and_return` set `status`='คืนแล้ว', `return_personel_id`='$personnelId', `return_staff_id`='$staffId', `return_date`='$newFormatReturnDate' WHERE `asset_id`= '" . $resp['id'] . "' AND `borrow_and_return_id` = '$borrowId'");
+        foreach ($asstVal as $key => $val) {
+            $stmt = $db->sqlQuery("UPDATE `detail_borrow_and_return` set `status`='คืนแล้ว', `return_personel_id`='$personnelId', `return_staff_id`='$staffId', `return_date`='$newFormatReturnDate' WHERE `asset_id`= '" . $asstVal[$key] . "' AND `borrow_and_return_id` = '$borrowId'");
             $stmt->execute();
-            $stmt = $db->sqlQuery("UPDATE `assets` set `status`='อยู่ในคลัง' WHERE `id`='" . $resp['id'] . "'");
+            $stmt = $db->sqlQuery("UPDATE `assets` set `status`='อยู่ในคลัง' WHERE `id`='" . $asstVal[$key] . "'");
             $stmt->execute();
         }
          header("location: ../../../../../project/views/borrow-return/borrow-return-management.php");
